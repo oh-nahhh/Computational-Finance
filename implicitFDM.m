e83b0864b6a978a89592151eeaf6d0f6133fc7bb
oh-nahhh <az09_109@hotmail.com>
2020-11-12 02:51:27 +0100

    FDM

--- implicitFDM.m
+++ implicitFDM.m
@@ -1,3 +1,5 @@
+%Group 1 - Assignment 3
+
 function [v,sv] = implicitFDM(K,r,sigma,T,M,N)
 %Allocate
 B = zeros(M-1,1);
@@ -20,15 +22,14 @@ n = N;
 while n >1
  %Euler backwards in time
  for j = 2:(M-1)
- alpha(j) = - 0.5*ss*s(j)^(2*gamma)*dt/(ds*ds) + 0.5*r*s(j)*dt/ds;
- beta(j) = 1 + ss*s(j)^(2*gamma)*dt/(ds*ds) + dt*r;
- zeta(j) = - 0.5*r*s(j)*dt/ds - 0.5*ss*s(j)^(2*gamma)*dt/(ds*ds);
+   alpha(j) = - 0.5*ss*s(j)^(2*gamma)*dt/(ds*ds) + 0.5*r*s(j)*dt/ds;
+   beta(j) = 1 + ss*s(j)^(2*gamma)*dt/(ds*ds) + dt*r;
+   zeta(j) = - 0.5*r*s(j)*dt/ds - 0.5*ss*s(j)^(2*gamma)*dt/(ds*ds);
  end
  %tridiagonal matrix, A
  A(1,1) = 1; %first element of A
  for j = 2:M-2
- A(j,j-1:j+1) = [alpha(j) beta(j) zeta(j)]; %fill the diagonals of
-A
+   A(j,j-1:j+1) = [alpha(j) beta(j) zeta(j)]; %fill the diagonals of A
  end
 
 A(M-1,M-2:M-1) = [alpha(M-1) beta(M-1)]; %last elements of A
